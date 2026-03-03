827ec53d1fd4d1f27fed7d0ddf54de13017e5515
athyfr <athyfr@proton.me>
2026-03-03 08:57:57 -0800

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,67 @@
+from matrix import Matrix
+
 running: bool = True
+
+matrix: Matrix = Matrix([[1.0, 0.0], [0.0, 1.0]], False)
 
 while running:
     print("What would you like to do?")
-    print("1: Add row")
-    print("2: Subtract row")
-    print("3: Multiply row")
-    print("4: Divide row")
+    print("1: Check current matrix")
+    print("2: Replace matrix")
+    print("3: Replace matrix cell")
+    print("4: Add row")
+    print("5: Subtract row")
+    print("6: Multiply row")
+    print("7: Divide row")
+    print("8: Swap row")
+    print("9: Gaussian elimination")
+
+    choice: int
+    try:
+        choice = int(input("Enter number here: "))
+        if choice < 1 or choice > 9:
+            print("Choice number out of range! Try again!")
+            break
+    except ValueError:
+        print("That wasn't a number! Try again!")
+        break
+
+    match choice:
+        case 1:
+            print("Printing matrix...")
+
+            for row in range(matrix.size):
+                print("[", end="  ")
+                for col in range(matrix.size):
+                    print(matrix.data[col][row], sep="", end="  ")
+                if matrix.augmented:
+                    print("|", matrix.data[matrix.size][row], "]")
+                else:
+                    print("]")
+        case 2:
+            print("Not yet implemented..")
+        case 3:
+            print("Not yet implemented..")
+        case 4:
+            print("Not yet implemented..")
+        case 5:
+            print("Not yet implemented..")
+        case 6:
+            print("Not yet implemented..")
+        case 7:
+            print("Not yet implemented..")
+        case 8:
+            print("Not yet implemented..")
+        case 9:
+            numsol: int = matrix.gaussian_elimination()
+
+            if not matrix.augmented:
+                break
+
+            match numsol:
+                case -1:
+                    print("Finished! No solutions.")
+                case 0:
+                    print("Finished! One solution.")
+                case _:
+                    print(f"Finished! {numsol} free variables.")
