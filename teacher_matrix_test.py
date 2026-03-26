a426f066237414044111c0dfd5477eb892c8819d
athyfr <athyfr@proton.me>
2026-03-26 09:09:42 -0700

    Move teacher's tests into separate file.
    Signed-off-by: athyfr <athyfr@proton.me>

---
+++ teacher_matrix_test.py
@@ -0,0 +1,15 @@
+import pytest
+
+import matrix
+
+# A wrapper for my implementation, with a signature copied from the
+# teacher's source files.
+def twoRowGausian(A=[[1,0,0],[0,1,0]]):
+    the_matrix: matrix.Matrix = matrix.Matrix()
+
+# Copied and pasted from teacher's OneDrive:
+def test_twoRowGausian():
+   assert twoRowGausian([[ 2,3,7],[ -4,6,-2]]) == ([[1.0, 0.0, 2.0], [0.0, 1.0, 1.0]], 0)
+   assert twoRowGausian([[ 2,3,7],[  4,6,-2]])[1] == 2
+   assert twoRowGausian([[ 2,3,7],[ 4,6,14]])[1] == 1
+   assert twoRowGausian([[ 2,3,12],[ -4,6,0]]) == ([[1.0, 0.0, 3.0], [0.0, 1.0, 2.0]], 0)
