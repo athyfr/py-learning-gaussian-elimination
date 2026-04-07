663f3a96d2112934afe0db4de4231510a4f21ea2
athyfr <athyfr@proton.me>
2026-04-07 09:16:57 -0700

    Disable Devenv LSP setup.

--- devenv.nix
+++ devenv.nix
@@ -9,6 +9,7 @@
   languages.python = {
     enable = true;
     package = pkgs.python314;
+    lsp.enable = false; # Devenv's LSP setup system doesn't meet my requirements.
     venv.enable = true;
     venv.requirements = ./requirements.txt;
   };
