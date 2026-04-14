4fa67499bdb5385621c326ac19e1192bf21e7342
athyfr <athyfr@proton.me>
2026-04-14 09:58:35 -0700

    Add test for ``Matrix.__init__``.

--- test_matrix.py
+++ test_matrix.py
@@ -6,3 +6,12 @@ import pytest
 from matrix import Matrix
 
 
+def test_init(
+    matrix_init_data_fixture: tuple[list[list[float]], bool],
+) -> None:
+    """Test ``__init__()`` for exceptions, and correct assignments."""
+    data, augmented = matrix_init_data_fixture
+    matrix = Matrix(matrix_init_data_fixture[0], matrix_init_data_fixture[1])
+
+    assert matrix.data == data
+    assert matrix.augmented == augmented
