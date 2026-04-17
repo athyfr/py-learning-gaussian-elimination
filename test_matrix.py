86e3626d755e1d0c318a5a605ae3bd4a9daa3928
athyfr <athyfr@proton.me>
2026-04-17 08:51:56 -0700

    Use mtft in test_matrix.py

--- test_matrix.py
+++ test_matrix.py
@@ -1,9 +1,10 @@
 """Test module for the Matrix class."""
 import matrix
+import matrix_types_for_tests as mtft
 
 
 def test_init(
-    matrix_init_data_fixture: tuple[list[list[float]], bool],
+    matrix_init_data_fixture: mtft.MatrixInitData,
 ) -> None:
     """Test ``__init__()`` for exceptions, and correct assignments."""
     data, augmented = matrix_init_data_fixture
