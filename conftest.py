f427776b75567a2acea37cdf7fd345c415c59a2c
athyfr <athyfr@proton.me>
2026-04-17 08:42:35 -0700

    Use custom type annotation in conftest.py

--- conftest.py
+++ conftest.py
@@ -3,6 +3,8 @@ from pathlib import Path
 
 import pytest
 
+import matrix_types_for_tests as mtft
+
 # Load example matrices from JSON file.
 matrices: dict[str, dict]
 with Path("test_matrix_cases.json").open() as f:
@@ -12,7 +14,7 @@ with Path("test_matrix_cases.json").open() as f:
 @pytest.fixture(params=matrices)
 def matrix_init_data_fixture(
     request: pytest.FixtureRequest,
-) -> tuple[list[list[float]], bool]:
+) -> mtft.MatrixInitData:
     """Get initializer data for a Matrix."""
     matrix_dict: dict = request.param
     return matrix_dict["data"], matrix_dict["augmented"]
