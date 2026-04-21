2eb18a2cbd84f5356d32bbf0bbce7a10af9be620
athyfr <athyfr@proton.me>
2026-04-21 09:50:40 -0700

    Move requirements to `pyproject.toml`

--- devenv.nix
+++ devenv.nix
@@ -11,6 +11,6 @@
     package = pkgs.python314;
     lsp.enable = false; # Devenv's LSP setup system doesn't meet my requirements.
     venv.enable = true;
-    venv.requirements = ./requirements.txt;
+    venv.requirements = ./pyproject.toml;
   };
 }
