726253d0638e97ad4503ac4ee3d35f7cbc5cee88
athyfr <athyfr@proton.me>
2026-04-27 09:22:07 -0700

    Correctly wire up import/read paths.

--- tests/matrix_test_cases.py
+++ tests/matrix_test_cases.py
@@ -3,5 +3,5 @@ import json
 from pathlib import Path
 
 matrices: dict[str, dict]
-with Path("matrix_test_cases.json").open() as f:
+with Path("tests/matrix_test_cases.json").open() as f:
     matrices: dict[str, dict] = json.load(f)
